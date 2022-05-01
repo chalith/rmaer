8e3f5b99839409a0ccebdbf0b2aa944a161adf96
Eashan Rajapaksha <aerajapakshe@gmail.com>
2022-05-01 17:33:27 +0530

    modified login and registerpages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { Title } from '@angular/platform-browser';
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
-  title = 'CoreUI Free Angular Admin Template';
+  title = 'PCMS - Photography Competition Management System';
 
   constructor(
     private router: Router,
